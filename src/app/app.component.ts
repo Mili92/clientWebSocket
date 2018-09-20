32311b72a47eeb9e5a550eb67d4b009fb55aa4e0
Mili <milirogbeer@hotmail.com>
2018-09-20 08:49:55 +0200

    clientWebSocket first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client-interface-ws';
+
+
+  /**
+ * Instancie un observable WebSocket
+ */
+private _socket: WebSocketSubject<any>;
+
+/**
+ * Tableau pour recevoir les msgs du server
+ */
+public serverMessages: String[];
+
+constructor() {
+  //Juste pour tester la communication sortante
+  this._send();
+  console.log('Connexion du client WebSocket');
+
+  this._socket = new WebSocketSubject('ws://127.0.0.1:8999');
+
+  //Souscription aux messages provenant du server
+  this._socket
+    .subscribe( (message)=>{
+      console.log('Le server envoie : '+ message);
+      this.serverMessages.push(message);
+    },
+    (err)=> console.error('Erreur levée : '+JSON.stringify(err)),
+    ()=>console.warn('Completed!')
+  );
 }
+
+private _send():void {
+  console.log('Envoie un nouveau message vers le serveur');
+  this._socket.next('Nouveau client connecté');
+}
+}
+
+
+
+
